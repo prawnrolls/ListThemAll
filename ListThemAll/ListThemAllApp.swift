c081a8a241ff0592cfc5d8bc4ce91208d8c10ebb
Linda Dang <lindadang801@gmail.com>
2025-02-21 10:31:24 +1100

    Initial Commit

---
+++ ListThemAll/ListThemAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListThemAllApp.swift
+//  ListThemAll
+//
+//  Created by Linda Dang on 21/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct ListThemAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
