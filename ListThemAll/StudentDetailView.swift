e72aa8e05c3f78e386977aec33d61bce9ee19a33
Linda Dang <lindadang801@gmail.com>
2025-02-21 11:50:11 +1100

    made child detailed view for "Linda" student page - hardcoded

---
+++ ListThemAll/StudentDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  StudentDetailView.swift
+//  ListThemAll
+//
+//  Created by Linda Dang on 21/2/2025.
+//
+
+import SwiftUI
+
+struct StudentDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StudentDetailView()
+}
